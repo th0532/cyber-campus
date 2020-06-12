b0f68c056a321b15245acbc975d9ce25400ecc37
th0532 <kkllpp123456@naver.com>
2020-06-13 02:04:52 +0900

    Fix Top, css Update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Top from './component/top';
 import Sidebar from './component/sidebar';
 import Content from './component/content';
 import Footer from './component/footer'
+import arrayData from "./data.json";
 
 class App extends React.Component {
   state = {
@@ -15,8 +16,11 @@ class App extends React.Component {
     intended:[]
   }
   getMainList = async() =>{
-    const data = await axios.get("http://recruit.dothome.co.kr/data.json");
-    this.setState({MainList:data.data.MainList, Info:data.data.Info, ingInfo:data.data.ingInfo, importantInfo:data.data.importantInfo, intended:data.data.intended})
+    // axios => https 문제 => local data 사용
+    // const data = await axios.get("http://recruit.dothome.co.kr/data.json");
+    // this.setState({MainList:data.data.MainList, Info:data.data.Info, ingInfo:data.data.ingInfo, importantInfo:data.data.importantInfo, intended:data.data.intended})
+    this.setState({MainList:arrayData.MainList, Info:arrayData.Info, ingInfo:arrayData.ingInfo, importantInfo:arrayData.importantInfo, intended:arrayData.intended})
+  
   }
   
   componentDidMount(){
