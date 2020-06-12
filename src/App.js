72f7e39afbb7ed590235b553d8ef5fc399155611
th0532 <kkllpp123456@naver.com>
2020-06-12 21:57:21 +0900

    Fix Content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 import Top from './component/top';
 import Sidebar from './component/sidebar';
 import Content from './component/content';
 
-function App() {
-  return (
-    <div className="App">
-        <Top/>
-        <div className="container">
-          <Sidebar/>
-          <Content/>
-        </div>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    MainList:[],
+    Info:[],
+    ingInfo:[]
+  }
+  getMainList = async() =>{
+    const data = await axios.get("http://recruit.dothome.co.kr/data.json");
+    this.setState({MainList:data.data.MainList, Info:data.data.Info, ingInfo:data.data.ingInfo})
+  }
+  
+  componentDidMount(){
+    this.getMainList();
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Top/>
+          <div className="container">
+            <Sidebar/>
+            <Content
+            MainList = {this.state.MainList} Info ={this.state.Info} ingInfo = {this.state.ingInfo}
+            />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/content.js
+++ src/component/content.js
@@ -2,26 +2,21 @@ import React from "react";
 import "./content.css";
 
     class Content extends React.Component{
-        render(){
+        render(props){
             return(
                 <div className="content_fakemargin">
                     <div className="content">
                         <div className="main_fakemargin">
                             <div className="main_wrap">
-                                <div className="explane">
-                                    <h3 className="title_color">강좌 전체보기 ▼</h3>
-                                    <span>화살표를 누르면 지난 학기 강좌 등을 볼 수 있습니다.</span>
-                                    <a className="setting">▣</a>
+                                <div className="subject">
+                                    <div className="subject_title">
+                                        <h3 className="title_color">강좌 전체보기 ▼</h3>
+                                        <span>화살표를 누르면 지난 학기 강좌 등을 볼 수 있습니다.</span>
+                                        <a className="setting">▣</a>
+                                    </div>
+                                        <MainList MainList = {this.props.MainList}/>
                                 </div>
-                                <MainList img={"http://recruit.dothome.co.kr/img/profile.png"} type1={"비정규"} type1Class={"TypePurple"} subject={"2020-1 ‘취·창업 진로세미나’ 8주차(입사서류/면접 완전정복 특강) 강좌 "} professor={"취업지원팀 / 박윤주"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor1.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"알고리즘 (08095_003)"} professor={"김우성"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor2.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"웹프로그래밍 (09079_003)"} professor={"이복기"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/profile.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"리눅스 (10888_007)"} professor={"유현"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor3.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"C++ (11581_002)"} professor={"김석수"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor4.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"창업과 마케팅 (12735_001)"} professor={"이상윤"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor5.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"웹 프로그래밍 입문 (13056_001)"} professor={"황희정"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/professor6.png"} type1={"교과"} type1Class={"TypeGreen"} type2={"글로벌"} type2Class={"TypeWhite"} subject={"취·창업 진로세미나 (13129_089)"} professor={"강운구"} />
-                                <MainList img={"http://recruit.dothome.co.kr/img/profile.png"} type1={"비정규"} type1Class={"TypePurple"} subject={"2020년도 재학생 대상 폭력예방교육(성폭력⋅가정폭력)(G) (1_prevention)"} professor={"가천인권센터"} />
+                                <Information Information={this.props.Info}/>
                             </div>
                         </div>
                         <div className="rightMenu">
@@ -31,29 +26,56 @@ import "./content.css";
                 </div>
             )
         }
+        
     }
-
     class MainList extends React.Component{
-        render(){
+        render(props){
             return(
                 <div className="mainList">
                     <ul>
-                        <li>
-                            <img src ={this.props.img}/>
+                        {this.props.MainList.map((data,index) =>(
+                        <li key={index}>
+                            <img src ={data.img}/>
                             <div className ="Type">
-                                <p className={this.props.type1Class}>{this.props.type1}</p>
-                                <p className={this.props.type2Class}>{this.props.type2}</p>
+                                <p className={data.type1Class}>{data.type1}</p>
+                                <p className={data.type2Class}>{data.type2}</p>
                             </div>
                             <div className="list_info">
-                                <h4>{this.props.subject}</h4>
-                                <p>{this.props.professor}</p>
+                                <h4>{data.subject}</h4>
+                                <p>{data.professor}</p>
                             </div>
                         </li>
+                    ))}
                     </ul>
                 </div>
             )
         }
     }
 
+    class Information extends React.Component{
+        render(props){
+            return (
+                <div className = "infomation">
+                    <div className="infomation_title">
+                        <ul>
+                            <li className="infomation_title_active">공지사항</li>
+                            <li className="infomation_title_disabled">진행 강좌 공지</li>
+                            <li className="">더보기</li>
+                        </ul>
+                    </div>
+                    <div class="infomation_list">
+                        <ul>
+                        {this.props.Information.map((data,index) =>( 
+                            <li key ={index}><a href="#">{data.title} <span>{data.date}</span></a></li>
+                        ))}
+                        </ul>
+                    </div>
+                </div>
+            )
+        }
+    }
 
 export default Content; 
+// {this.props.Information.Info.map((data,index) =>(
+//     <li><a href="#">{data.title} <span>{data.date}</span></a></li>
+// ))}
